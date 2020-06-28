f253ccc3870bb1eb6e158fda58028a7657e9e79a
lizuzsusil <susachhami@gmail.com>
2020-06-28 14:17:01 +0545

    footer added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict'
+
+$( document ).ready(function() {
+    $(".menu-item").hover(function(){
+        $(this).css("color", "#488eff");
+        }, function(){
+        $(this).css("color", "#232323");
+    });
+    
+    $(".client").hover(function(){
+        $(this).css("transform", "scale(0.90,0.90)");
+        }, function(){
+        $(this).css("transform", "scale(1,1)");
+      });
+});
