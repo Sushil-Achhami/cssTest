64d6e76387fd7063e03d2bc9e069fc6489541560
lizuzsusil <susachhami@gmail.com>
2020-06-28 14:27:09 +0545

    media query added

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@ $( document ).ready(function() {
     
     $(".client").hover(function(){
         $(this).css("transform", "scale(0.90,0.90)");
+        $(this).css("box-shadow", "15px 15px 20px rgba(72, 142, 255, 0.1)");
         }, function(){
         $(this).css("transform", "scale(1,1)");
+        $(this).css("box-shadow", "0px 0px 0px rgba(0, 0,0, 0)");
       });
 });
